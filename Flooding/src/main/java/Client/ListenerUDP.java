7017c6bbee97b6c3d607401487775e2e06af8e22
LucasFerrazNicolau <40740846+LucasFerrazNicolau@users.noreply.github.com>
2019-08-03 17:12:19 -0300

    First commit of ListenerUDP
    First implementation of the class. Based on Listener implementation in p2p-gossip-protocol.

--- Flooding/src/main/java/Client/ListenerUDP.java
+++ Flooding/src/main/java/Client/ListenerUDP.java
@@ -5,10 +5,52 @@
  */
 package Client;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import org.json.JSONObject;
+
 /**
  *
- * @author Jairo
+ * @author Lucas
  */
-public class ListenerUDP {
+public class ListenerUDP extends Thread {
+    private String IP;
+    private String port;
+    private MessageControllerUDP controller;
+    // need to implement timeout
     
+    public ListenerUDP(String IP, String port) {
+    	this.IP = IP;
+    	this.port = port;
+    	controller = new MessageControllerUDP();
+    }
+    
+    //Run method from Thread inheritance. Continuously loop receiving messages and treating them
+    public void run() {
+    	try {
+    		DatagramSocket socket = new DatagramSocket(Integer.parseInt(port));
+    		
+    		while (true) {
+    			// Verify buffer length
+    			byte[] buffer = new byte[1024];
+    			DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+    			
+    			socket.receive(packet);
+    			
+    			String serializedData = packet.getData().toString();
+    			
+    			JSONObject message = controller.deserializeMessage(serializedData);
+    			if (controller.validateMessage(message)) {
+    				if (controller.checkResponse(message)) {
+    					controller.startRequest(message);
+    				}
+    			}
+    		}
+    		
+    		//socket.close();
+    	} catch (Exception ex) {
+    		// Exception from socket
+    		System.out.println("Something went very wrong");
+    	}
+    }
 }
