91dfa7c7593b9a8c340c488e245f4eba8e2263ce
jairojuunior <jairodasilvafreitasjunior@hotmail.com>
2019-08-06 14:51:30 -0300

    Finished PeerDAO, FileSystemReader and GarbageCollector

--- Flooding/src/main/java/Peer/GarbageCollector.java
+++ Flooding/src/main/java/Peer/GarbageCollector.java
@@ -5,10 +5,41 @@
  */
 package Peer;
 
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
 /**
  *
  * @author Jairo
  */
-public class GarbageCollector {
+public class GarbageCollector extends Thread {
+    private final long refreshTime;
     
+    public GarbageCollector(long refreshTime){
+        this.refreshTime = refreshTime;
+    }
+    
+    @Override
+    public void run() {
+    	while (true) {
+    		PeerDAO peerDAO = PeerDAO.getInstance();
+    		ArrayList<String> keys = PeerDAO.INSTANCE.recentQueries.getAllKeys();
+    		
+    		keys.forEach(key->{
+                    int countdown = PeerDAO.INSTANCE.recentQueries.get(key).getBlacklistCount();
+                    if(countdown == 0){ //If countdown = 0 then remove query from blacklist
+                        PeerDAO.INSTANCE.recentQueries.delete(key);
+                    }else{
+                        PeerDAO.INSTANCE.recentQueries.get(key).decreaseCountdown();
+                    }
+                });
+                        
+    		//Wait for next run
+    		try {
+    			TimeUnit.SECONDS.sleep(refreshTime);
+    		} catch (InterruptedException e) {
+    			System.out.println("Error ocurred while executing sleep method");
+    		}
+    	}
+    }
 }
--- Flooding/src/main/java/Peer/Main.java
+++ Flooding/src/main/java/Peer/Main.java
@@ -5,6 +5,15 @@
  */
 package Peer;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
 /**
  *
  * @author Jairo
@@ -13,9 +22,22 @@ public class Main {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        
+        Stream<Path> local_files = Files.find(Paths.get("C:\\Users\\Jairo\\codes\\loft-case\\data"),
+                    Integer.MAX_VALUE,
+                    (filePath, fileAttr) -> fileAttr.isRegularFile());
+        
+        local_files.forEach(fullpath->{
+            try {
+                System.out.println(Files.readAttributes(fullpath, BasicFileAttributes.class).size());
+            } catch (IOException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
     }
     
 }
